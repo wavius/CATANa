baa0221e2e110213b087bcbceecec226ed79dc92
Noah Lasorda <nlasorda@gmail.com>
2025-07-01 14:57:14 -0400

    minor tweaks/bug fixes

--- core/Main.py
+++ core/Main.py
@@ -10,9 +10,25 @@ net_turn = 0
 print("-----------------starting game-----------------")
 
 # Game loop
-while p1.vic_points < 10 and p2.vic_points < 10 and p3.vic_points < 10 and p4.vic_points < 10 and net_turn < 210:
-    print("-----------------turn ", game_turn, " -----------------")
-    if game_turn == 0:
+while p1.vic_points < 10 and p2.vic_points < 10 and p3.vic_points < 10 and p4.vic_points < 10 and net_turn < 200:
+    print("----------------------------------")
+    if net_turn < 2:
+        print("start turn")
+        if game_turn == 0:
+            # Player 1's turn (start turns)
+            pass
+        elif game_turn == 1:
+            # Player 2's turn (start turns)
+            pass
+        elif game_turn == 2:
+            # Player 3's turn (start turns)
+            pass
+        elif game_turn == 3:
+            # Player 4's turn (start turns)
+            pass
+        else:
+            print("Error: Invalid game turn number. (start turns)")
+    elif game_turn == 0:
         # Player 1's turn
         pass
     elif game_turn == 1:
@@ -29,8 +45,8 @@ while p1.vic_points < 10 and p2.vic_points < 10 and p3.vic_points < 10 and p4.vi
         
 
     #turn ticker
-    net_turn += 1
+    print("End of turn ", net_turn, " for player ", game_turn + 1)
     game_turn += 1
     if game_turn > 3:
+        net_turn += 1
         game_turn = 0
-    print("End of turn ", net_turn, " for player ", game_turn + 1)
